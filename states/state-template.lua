4a696c03111799f7f2f65732789105dd90e91c8f
Vasco Ivo Prata Massano Esteves Correia <maxteel1@gmail.com>
2013-04-27 02:48:30 +0100

    State template
    According to the state manager

---
+++ states/state-template.lua
@@ -0,0 +1,60 @@
+-- 
+-- STATE TEMPLATE --
+--
+
+local state = {}
+local state.layerTable = nil
+
+-- partitions
+state.main_partition = nil
+state.gui_partition = nil
+
+-- IO Stuff
+state.mouseX
+state.mouseY
+
+--GUI stuff
+state.gui = nil  
+
+
+------ StateManager functions ------
+function state.onFocus(self)
+
+end
+
+
+function state.onLoad(self)
+
+end
+
+
+function state.onInput(self)
+	-- I think I'll manage the inpu myself
+end
+
+
+function state.onUnload(self)
+
+end
+
+
+function state.onLoseFocus(self)
+
+end
+
+
+
+-- Functionality implementation
+
+
+
+-- Input Callbacks (maybe)
+
+
+
+-- state specific utils
+
+
+
+
+return state
